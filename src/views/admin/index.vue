b72294cc15d71da91864e9f05dd9cb370ceedd66
robinWu <andyking27@163.com>
2018-11-02 15:15:06 +0800

    rebuild

--- src/views/admin/index.vue
+++ src/views/admin/index.vue
@@ -7,27 +7,11 @@
             <a href="javascript:void(0)" @click="alert.dismissCountDown=0"><i class="fa fa-times"></i></a>
         </b-alert>
         <b-navbar toggleable="md" type="dark" variant="secondary">
-            <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
             <b-navbar-brand href="#">N-Boke 后台管理系统</b-navbar-brand>
-            <b-collapse is-nav id="nav_collapse">
-                <!--<b-navbar-nav>-->
-                    <!--<b-nav-item href="#">Link</b-nav-item>-->
-                    <!--<b-nav-item href="#" disabled>Disabled</b-nav-item>-->
-                <!--</b-navbar-nav>-->
+            <div style="flex-grow: 1"  v-if="admin_info">
+                <b-button variant="default" size="mini" @click="signout" class="float-right"><i class="fa fa-sign-out"></i></b-button>
+            </div>
 
-                <!-- Right aligned nav items -->
-                <b-navbar-nav class="ml-auto">
-
-                    <b-nav-item-dropdown v-if="admin_info" right>
-                        <!-- Using button-content slot -->
-                        <template slot="button-content">
-                            <em>{{admin_info.name}}</em>
-                        </template>
-                        <b-dropdown-item href="#">Profile</b-dropdown-item>
-                        <b-dropdown-item @click="signout()">Signout</b-dropdown-item>
-                    </b-nav-item-dropdown>
-                </b-navbar-nav>
-            </b-collapse>
         </b-navbar>
             <!-- Stack the columns on mobile by making one full-width and the other half-width -->
         <transition name="slide"><router-view class="view"></router-view></transition>
